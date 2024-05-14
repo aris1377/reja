75af017301d7fbd2b91e1387eebe9042a891c104
aris1377 <bek77519101@gmail.com>
2024-05-14 19:04:37 +0900

    fix: modifiy server.js

--- server.js
+++ server.js
@@ -11,16 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 //2.session code
 
 //3.views code
-app.set("view", "view");
+app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4.routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style = "background:red">Hello World</h1>`);
-});
-
-app.get("/gift", function (req, res) {
-  res.end("Siz sovg'alar bo'limidasiz");
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 //server xosil qilamiz
