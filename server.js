43a6bd3cc43ae12ef135cf9a5d905343c5da1cf0
aris1377 <bek77519101@gmail.com>
2024-05-24 03:08:23 +0900

    fix: refactor

--- server.js
+++ server.js
@@ -30,12 +30,14 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function (req, res) {
-  res.render("harid");
+  res.render("reja");
 });
 
 //server xosil qilamiz
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The server is running successfully on port:${PORT}`);
+  console.log(
+    `The server is running successfully on port:${PORT},http://localhost:${PORT}`
+  );
 });
